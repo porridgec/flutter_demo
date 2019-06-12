ecf6e491185251898caec1bef74bddd6804bf82a
Hahn Chan <chensihan@laiqutech.com>
2019-06-12 14:39:24 +0800

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'RandomWords.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,112 +8,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "BBBBB is coming",
       home: new RandomWords(),
       theme: new ThemeData(
         primaryColor: Colors.white,
       ),
     );
+    // return new CupertinoApp(
+    //   debugShowCheckedModeBanner: false,
+    //   title: "B's CupertinoApp Demo",
+    //   home: new MainPage(),
+    //   theme: new CupertinoThemeData(
+    //     primaryColor: CupertinoColors.activeBlue,
+    //   ),
+    // );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return new RandomWordsState();
-  }
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = new Set<WordPair>();
+class MainPage extends StatelessWidget {
+  final tabs = ["视频","我的"];
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
-    // return new Text(wordPair.asPascalCase);
-    // _suggestions.add(new WordPair.random());
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('B generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list),
-          onPressed: _pushSaved),
+    // TODO: implement build
+    return new CupertinoTabScaffold(
+      tabBar: new CupertinoTabBar(
+        items: [
+          new BottomNavigationBarItem(
+            title: new Text(tabs[0]),
+            icon: new Icon(CupertinoIcons.video_camera),
+          ),
+          new BottomNavigationBarItem(
+            title: new Text(tabs[1]),
+            icon: new Icon(CupertinoIcons.person),
+          )
         ],
       ),
-      body: _buildSuggestions(),
+      tabBuilder: (context, i) {
+        return CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(
+            middle: new Text(tabs[i]),
+          ),
+          child: Center(
+            child: new Text("now in ${tabs[i]}"),
+          ),
+        );
+      }
     );
   }
 
-  void _pushSaved() {
-    print("push it!");
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divied = ListTile
-          .divideTiles(
-            context: context,
-            tiles: tiles
-          )
-          .toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text("saved"),
-            ),
-            body: new ListView(children: divied,),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (contex, i) {
-        if (i.isOdd) return new Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon (
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
 }
+
